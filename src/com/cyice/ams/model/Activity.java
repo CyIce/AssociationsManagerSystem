cab3d1c4c626992b745ae892f42bf54a27756f46
CyIce <1207201395@qq.com>
2017-11-06 22:25:35 +0800

    有关model的三个类

---
+++ src/com/cyice/ams/model/Activity.java
@@ -0,0 +1,14 @@
+package com.cyice.ams.model;
+
+public class Activity {
+
+	//活动名称
+	private String name;
+	//活动时间
+	private String time;
+	//活动地点
+	private String place;
+	//活动简介
+	private String synopsis;
+	
+}
---
+++ src/com/cyice/ams/model/Association.java
@@ -0,0 +1,14 @@
+package com.cyice.ams.model;
+
+public class Association {
+
+	//社团名称
+	private String name;
+	//负责人
+	private People charge;
+	//成立时间
+	private String foundingTime;
+	//简介
+	private String synopsis;
+	
+}
