cc9e8871fa4a4d881697c699d7e052e4f760faac
CyIce <1207201395@qq.com>
2017-11-11 12:58:47 +0800

    切换AssociationView的布局方式为GroupLayout，将事件提监听函数移至Control类中。

--- src/com/cyice/ams/control/Control.java
+++ src/com/cyice/ams/control/Control.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import com.cyice.ams.model.Association;
 import com.cyice.ams.model.FileInput;
 import com.cyice.ams.view.AssociationsView;
 
-public class Control {
+public class Control implements ListSelectionListener {
 
-	//社团介绍实图实例
+	// 社团介绍实图实例
 	private AssociationsView associationsView;
 	// 社团实例
 	private List<Association> associations = new ArrayList<>();
@@ -26,13 +29,20 @@ public class Control {
 				try {
 					associationsView = new AssociationsView();
 					associationsView.setVisible(true);
+					initSelectetionListener();
 					initJList();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-	
+
+	}
+
+	// 绑定事件监听函数
+	private void initSelectetionListener() {
+		associationsView.getAssociations().addListSelectionListener(this);
+		associationsView.getActivities().addListSelectionListener(this);
 	}
 
 	// 初始化社团实例
@@ -48,23 +58,32 @@ public class Control {
 		}
 	}
 
-	//初始化社团和活动列表
+	// 初始化社团和活动列表
 	private void initJList() {
-		DefaultListModel<String> associationsValue=new DefaultListModel<>();
-		DefaultListModel<String> activitiesValue=new DefaultListModel<>();
-		for (Association association:associations) {
+		DefaultListModel<String> associationsValue = new DefaultListModel<>();
+		DefaultListModel<String> activitiesValue = new DefaultListModel<>();
+		for (Association association : associations) {
 			associationsValue.addElement(association.getName());
 			activitiesValue.addElement(association.getActivities());
 		}
 		associationsView.getAssociations().setModel(associationsValue);
 		associationsView.getActivities().setModel(activitiesValue);
-		
+
 	}
-	
-	
-	
+
 	public List<Association> getAssociations() {
 		return associations;
 	}
 
+	// 事件监听函数
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+
+		if (e.getSource().equals(associationsView.getAssociations())) {
+			associationsView.getAssociationSearch().setText((String) ((JList<?>) e.getSource()).getSelectedValue());
+
+		} else if (e.getSource().equals(associationsView.getActivities())) {
+			associationsView.getActivitySearch().setText((String) ((JList<?>) e.getSource()).getSelectedValue());
+		}
+	}
 }
--- src/com/cyice/ams/view/AssociationsView.java
+++ src/com/cyice/ams/view/AssociationsView.java
@@ -23,73 +23,93 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextArea;
 
-public class AssociationsView extends JFrame implements ListSelectionListener {
-
-	// 社团和活动的JList
+public class AssociationsView extends JFrame{
 	private JList<String> associations;
 	private JList<String> activities;
-	// 社团和活动的信息
-	private JTextField infoArea;
-	//用于判断鼠标是否点击社团或活动
-	private boolean isClickAssociation=false;
-	private boolean isClickActivity=false;
-	//储存点击的社团名或活动名
-	private String clickedAssociation;
-	private String clickedActivity;
+	private JTextField associationSearch;
+	private JTextField activitySearch;
+	private JTextArea infoArea;
 
 	public AssociationsView() {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 900, 600);
-		getContentPane().setLayout(null);
-
-		JScrollPane associationsSP = new JScrollPane();
-		associationsSP.setBounds(0, 100, 290, 300);
-		getContentPane().add(associationsSP);
-
-		associations = new JList<String>();
-		associations.addListSelectionListener(this);
-		associations.setBackground(new Color(176, 224, 230));
-		associations.setFont(new Font("Kokonor", Font.PLAIN, 18));
-		associationsSP.setViewportView(associations);
-
-		JScrollPane activitiesSP = new JScrollPane();
-		activitiesSP.setBounds(592, 100, 290, 300);
-		getContentPane().add(activitiesSP);
-
-		activities = new JList<String>();
-		activities.addListSelectionListener(this);
-		activities.setBackground(new Color(176, 224, 230));
-		activities.setFont(new Font("Kokonor", Font.PLAIN, 18));
-		activitiesSP.setViewportView(activities);
-
-		JTextField associationSearch = new JTextField();
-		associationSearch.setBounds(100, 66, 190, 35);
-		getContentPane().add(associationSearch);
+		
+		JLabel associationSearchLabel = new JLabel("  搜索  ");
+		
+		associationSearch = new JTextField();
 		associationSearch.setColumns(10);
-
-		JLabel associationSearchLabel = new JLabel("        搜索");
-		associationSearchLabel.setBounds(0, 66, 85, 35);
-		getContentPane().add(associationSearchLabel);
-
-		JLabel activitySearchLabel = new JLabel("        搜索");
-		activitySearchLabel.setBounds(593, 66, 85, 35);
-		getContentPane().add(activitySearchLabel);
-
-		JTextField activitySearch = new JTextField();
-		activitySearch.setBounds(692, 66, 190, 35);
-		getContentPane().add(activitySearch);
+		
+		JScrollPane associationSP = new JScrollPane();
+		
+		JLabel activitySearchLabel = new JLabel("  搜索。");
+		
+		activitySearch = new JTextField();
 		activitySearch.setColumns(10);
-
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(591, 66, -300, 334);
-		getContentPane().add(scrollPane);
-
-		infoArea = new JTextField();
-		infoArea.setBounds(289, 100, 305, 300);
-		getContentPane().add(infoArea);
-		infoArea.setColumns(10);
+		
+		JScrollPane activitySP = new JScrollPane();
+		
+		JScrollPane infoAreaSP = new JScrollPane();
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(associationSearchLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(associationSearch, GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE))
+						.addComponent(associationSP, GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(infoAreaSP, GroupLayout.DEFAULT_SIZE, 366, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(activitySearchLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(activitySearch))
+						.addComponent(activitySP, GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(41)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(associationSearchLabel, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+						.addComponent(associationSearch, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+						.addComponent(activitySearchLabel)
+						.addComponent(activitySearch, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(associationSP, GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE)
+						.addComponent(infoAreaSP, Alignment.TRAILING)
+						.addComponent(activitySP))
+					.addContainerGap(224, Short.MAX_VALUE))
+		);
+		
+		infoArea = new JTextArea();
+		infoArea.setEditable(false);
+		infoAreaSP.setViewportView(infoArea);
+		
+		activities = new JList();
+		activities.setBackground(new Color(0, 255, 255));
+		activities.setForeground(new Color(0, 0, 51));
+		activities.setFont(new Font("Kokonor", Font.BOLD, 18));
+		activitySP.setViewportView(activities);
+		
+		associations = new JList();
+		associations.setBackground(new Color(0, 255, 255));
+		associations.setForeground(new Color(0, 0, 51));
+		associations.setFont(new Font("Kokonor", Font.BOLD, 18));
+		associationSP.setViewportView(associations);
+		getContentPane().setLayout(groupLayout);
 
 	}
 
@@ -101,36 +121,15 @@ public class AssociationsView extends JFrame implements ListSelectionListener {
 		return activities;
 	}
 
-	public JTextField getInfoArea() {
+	public JTextArea getInfoArea() {
 		return infoArea;
 	}
 
-	public boolean isClickAssociation() {
-		return isClickAssociation;
+	public JTextField getAssociationSearch() {
+		return associationSearch;
 	}
 
-	public boolean isClickActivity() {
-		return isClickActivity;
+	public JTextField getActivitySearch() {
+		return activitySearch;
 	}
-
-	public String getClickedAssociation() {
-		return clickedAssociation;
-	}
-
-	public String getClickedActivity() {
-		return clickedActivity;
-	}
-
-	//事件监听函数
-	@Override
-	public void valueChanged(ListSelectionEvent e) {
-
-		if(e.getSource().equals(associations)) {
-			System.out.println(((JList<?>)e.getSource()).getSelectedValue());
-		}else if(e.getSource().equals(activities)) {
-			System.out.println(((JList<?>)e.getSource()).getSelectedValue());
-		}
-	}
-	
-	
 }
